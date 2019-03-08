9de657c43c4a9294d77e110376b21fba55c00866
kalpeshs007 <kalpeshsawant78@gmail.com>
2019-03-08 16:40:30 +0530

    form refactor

---
+++ src/components/B2C/MainArea/Header/HeaderLeft/DomesticForm/DomesticForm.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import { TabPanel } from "react-tabs";
+import { Container, Row, Col, FormGroup, Label, Input } from "reactstrap";
+import { Icon } from "react-icons-kit";
+import { ic_compare_arrows } from "react-icons-kit/md";
+import "./domesticformstyle.css";
+import "react-tabs/style/react-tabs.css";
+
+const DomesticForm = () => {
+  const handleRadionChange = e => {
+    console.log(e);
+  };
+  return (
+    <React.Fragment>
+      <div className="book-text-sp">
+        <p>Book Domestic Flight Tickets</p>
+      </div>
+
+      <div className="form-wrapper-sp">
+        <form>
+          <div className="d-flex">
+            <label className="container-spt">
+              One Way
+              <input
+                type="radio"
+                checked="checked"
+                name="radio"
+                onChange={handleRadionChange}
+              />
+              <span className="checkmark" />
+            </label>
+            <label className="container-spt">
+              Two City
+              <input type="radio" name="radio" />
+              <span className="checkmark" />
+            </label>
+            <label className="container-spt">
+              Multi City
+              <input type="radio" name="radio" />
+              <span className="checkmark" />
+            </label>
+          </div>
+
+          <Container fluid>
+            <Row style={{ marginTop: 10 }}>
+              <Col xl="5" className="col-headerLeft-form-spt">
+                <FormGroup>
+                  <Label for="from" className="lable-header-form-spt">
+                    From
+                  </Label>
+                  <Input
+                    type="text"
+                    name="from"
+                    placeholder="Any worldwide city or airport"
+                    className="input-header-spt"
+                  />
+                </FormGroup>
+              </Col>
+              <Col
+                xl="1"
+                className="col-headerLeft-form-spt d-flex justify-content-center align-items-center"
+              >
+                <div
+                  style={{
+                    width: 23,
+                    height: 23,
+                    color: "#022d41"
+                  }}
+                >
+                  <Icon size={"100%"} icon={ic_compare_arrows} />
+                </div>
+              </Col>
+              <Col xl="5" className="col-headerLeft-form-spt">
+                <FormGroup>
+                  <Label for="to" className="lable-header-form-spt">
+                    To
+                  </Label>
+                  <Input
+                    type="text"
+                    name="to"
+                    placeholder="Any worldwide city or airport"
+                    className="input-header-spt"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Container>
+
+          <Container fluid>
+            <Row style={{ marginTop: 5 }}>
+              <Col xl="4" className="col-headerLeft-form-spt">
+                <FormGroup>
+                  <Label for="date" className="lable-header-form-spt">
+                    Depart On
+                  </Label>
+                  <Input
+                    type="date"
+                    name="date"
+                    placeholder="Pick a Date"
+                    className="input-header-spt"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Container>
+
+          <Container fluid>
+            <Row style={{ marginTop: 5 }}>
+              <Col xl="3" className="col-headerLeft-form-spt">
+                <FormGroup>
+                  <Label for="adults" className="lable-header-form-spt">
+                    Adults
+                  </Label>
+                  <Input
+                    type="number"
+                    name="adults"
+                    placeholder="1"
+                    className="input-header-spt"
+                  />
+                </FormGroup>
+              </Col>
+              <Col xl="1" />
+              <Col xl="3" className="col-headerLeft-form-spt">
+                <FormGroup>
+                  <Label for="childrens" className="lable-header-form-spt">
+                    Childrens
+                  </Label>
+                  <Input
+                    type="number"
+                    name="childrens"
+                    placeholder="1"
+                    className="input-header-spt"
+                  />
+                </FormGroup>
+              </Col>
+              <Col xl="1" />
+              <Col xl="3" className="col-headerLeft-form-spt">
+                <FormGroup>
+                  <Label for="infants" className="lable-header-form-spt">
+                    Infants
+                  </Label>
+                  <Input
+                    type="number"
+                    name="infants"
+                    placeholder="1"
+                    className="input-header-spt"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Container>
+          <div className="divider-header-wrapper-spt">
+            <div className="divider-header-spt" />
+          </div>
+          <div className="button-header-wrapper-sp d-flex justify-content-end">
+            <button
+              type="submit"
+              className="btn btn-primary button-header-spt mt-2"
+            >
+              Search Flight
+            </button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default DomesticForm;
--- src/components/B2C/MainArea/Header/HeaderLeft/HeaderLeft.jsx
+++ src/components/B2C/MainArea/Header/HeaderLeft/HeaderLeft.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import "./headerleftstyle.css";
+import DomesticForm from "./DomesticForm/DomesticForm";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { Container, Row, Col, FormGroup, Label, Input } from "reactstrap";
-import { Icon } from "react-icons-kit";
-import { ic_compare_arrows } from "react-icons-kit/md/ic_compare_arrows";
+import "./headerleftstyle.css";
 import "react-tabs/style/react-tabs.css";
 
 const HeaderLeft = () => {
-  const handleRadionChange = e => {
-    console.log(e);
-  };
   return (
     <div className="headerleft-wrapper-spt">
       <Tabs className="tab-spt">
@@ -17,158 +12,8 @@ const HeaderLeft = () => {
           <Tab>Domestic</Tab>
           <Tab>International</Tab>
         </TabList>
-
         <TabPanel>
-          <div className="book-text-sp">
-            <p>Book Domestic Flight Tickets</p>
-          </div>
-
-          <div className="form-wrapper-sp">
-            <form>
-              <div className="d-flex">
-                <label className="container-spt">
-                  One Way
-                  <input
-                    type="radio"
-                    checked="checked"
-                    name="radio"
-                    onChange={handleRadionChange}
-                  />
-                  <span className="checkmark" />
-                </label>
-                <label className="container-spt">
-                  Two City
-                  <input type="radio" name="radio" />
-                  <span className="checkmark" />
-                </label>
-                <label className="container-spt">
-                  Multi City
-                  <input type="radio" name="radio" />
-                  <span className="checkmark" />
-                </label>
-              </div>
-
-              <Container fluid>
-                <Row style={{ marginTop: 10 }}>
-                  <Col xl="5" className="col-headerLeft-form-spt">
-                    <FormGroup>
-                      <Label for="from" className="lable-header-form-spt">
-                        From
-                      </Label>
-                      <Input
-                        type="text"
-                        name="from"
-                        placeholder="Any worldwide city or airport"
-                        className="input-header-spt"
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col
-                    xl="1"
-                    className="col-headerLeft-form-spt d-flex justify-content-center align-items-center"
-                  >
-                    <div
-                      style={{
-                        width: 23,
-                        height: 23,
-                        color: "#022d41"
-                      }}
-                    >
-                      <Icon size={"100%"} icon={ic_compare_arrows} />
-                    </div>
-                  </Col>
-                  <Col xl="5" className="col-headerLeft-form-spt">
-                    <FormGroup>
-                      <Label for="to" className="lable-header-form-spt">
-                        To
-                      </Label>
-                      <Input
-                        type="text"
-                        name="to"
-                        placeholder="Any worldwide city or airport"
-                        className="input-header-spt"
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-              </Container>
-
-              <Container fluid>
-                <Row style={{ marginTop: 5 }}>
-                  <Col xl="4" className="col-headerLeft-form-spt">
-                    <FormGroup>
-                      <Label for="date" className="lable-header-form-spt">
-                        Depart On
-                      </Label>
-                      <Input
-                        type="date"
-                        name="date"
-                        placeholder="Pick a Date"
-                        className="input-header-spt"
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-              </Container>
-
-              <Container fluid>
-                <Row style={{ marginTop: 5 }}>
-                  <Col xl="3" className="col-headerLeft-form-spt">
-                    <FormGroup>
-                      <Label for="adults" className="lable-header-form-spt">
-                        Adults
-                      </Label>
-                      <Input
-                        type="number"
-                        name="adults"
-                        placeholder="1"
-                        className="input-header-spt"
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col xl="1" />
-                  <Col xl="3" className="col-headerLeft-form-spt">
-                    <FormGroup>
-                      <Label for="childrens" className="lable-header-form-spt">
-                        Childrens
-                      </Label>
-                      <Input
-                        type="number"
-                        name="childrens"
-                        placeholder="1"
-                        className="input-header-spt"
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col xl="1" />
-                  <Col xl="3" className="col-headerLeft-form-spt">
-                    <FormGroup>
-                      <Label for="infants" className="lable-header-form-spt">
-                        Infants
-                      </Label>
-                      <Input
-                        type="number"
-                        name="infants"
-                        placeholder="1"
-                        className="input-header-spt"
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-              </Container>
-              <div className="divider-header-wrapper-spt">
-                <div className="divider-header-spt" />
-              </div>
-              <div className="button-header-wrapper-sp d-flex justify-content-end">
-                <button
-                  type="submit"
-                  className="btn btn-primary button-header-spt mt-2"
-                >
-                  Search Flight
-                </button>
-              </div>
-            </form>
-          </div>
+          <DomesticForm />
         </TabPanel>
         <TabPanel>
           <h2>Any content 2</h2>
