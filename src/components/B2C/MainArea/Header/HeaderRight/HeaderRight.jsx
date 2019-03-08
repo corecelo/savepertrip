7162c070a558848d078f78b6d1bc1a1390f66d00
Ramkrishna <psiddhesh14@gmail.com>
2019-03-08 12:24:48 +0530

    Right Side Created

--- src/components/B2C/MainArea/Header/HeaderRight/HeaderRight.jsx
+++ src/components/B2C/MainArea/Header/HeaderRight/HeaderRight.jsx
@@ -3,8 +3,23 @@ import "./headerrightstyle.css";
 
 const HeaderRight = () => {
   return (
-    <div className="headerright-wrapper-spt">
-      <h6>Right Column</h6>
+    <div className="headerright-wrapper-spt d-flex flex-column justify-content-start align-items-center pt-3">
+      <div className="slider-header-wrapper-spt" />
+      <div className="text-header-ad-wrapper mt-2 align-self-start pl-3">
+        <h6>Save with our Deals</h6>
+        <div className="divider-header-spt" />
+        <p className="mt-2 offer-text-spt">Internationals Flight's Offer</p>
+        <p className="offer-sub-text-spt">
+          Grab up to Rs.15,000 Instant Cashback on International Flights. Use
+          Coupon FESTIVE.
+        </p>
+      </div>
+      <a
+        href="#"
+        className="corporate-login-spt mt-4 d-flex flex-column justify-content-center align-items-center"
+      >
+        Corporate Login
+      </a>
     </div>
   );
 };
